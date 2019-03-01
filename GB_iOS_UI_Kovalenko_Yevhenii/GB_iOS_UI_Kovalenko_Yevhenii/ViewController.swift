d4cdeec49bbb9ef610e03a8181ad130bcd18939c
Vence <venceque@gmail.com>
2019-03-01 17:52:08 +0200

    project created

---
+++ GB_iOS_UI_Kovalenko_Yevhenii/GB_iOS_UI_Kovalenko_Yevhenii/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GB_iOS_UI_Kovalenko_Yevhenii
+//
+//  Created by Vence Berg on 01/03/2019.
+//  Copyright © 2019 Vence Berg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
